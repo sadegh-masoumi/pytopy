03e4a1c1db656e5cb76709606bd3e272b335130f
mohammad sadegh masoumi <sadegh.masoumi.ms@gmail.com>
2022-02-01 17:07:39 +0330

    test

--- core/settings/production.py
+++ core/settings/production.py
@@ -2,45 +2,23 @@ from core.settings.base import *
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-is_test_net = os.environ.get('is_test_net')
-if is_test_net == 'False':
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'pytopyir_PyDatabase',
-            'USER': 'pytopyir_PyToPy_main',
-            'PASSWORD': '^6zC9359005490.ms',
-            'HOST': 'localhost',
-            'POST': '3306',
-            'OPTIONS': {
-                'sql_mode': 'STRICT_ALL_TABLES'
-            }
+
+ALLOWED_HOSTS = [
+    '*'
+]
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pytopyir_test_net',
+        'USER': 'pytopyir_test_net_user',
+        'PASSWORD': 'r2+1}aWu~Bk]',
+        'HOST': 'localhost',
+        'POST': '3306',
+        'OPTIONS': {
+            'sql_mode': 'STRICT_ALL_TABLES'
         }
     }
-    ALLOWED_HOSTS = [
-        'pytopy.ir',
-        'http://pytopy.ir/',
-        'https://pytopy.ir/',
-        'http://www.pytopy.ir/',
-        'https://www.pytopy.ir/',
-    ]
-else:
-    ALLOWED_HOSTS = [
-        '*'
-    ]
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'pytopyir_test_net',
-            'USER': 'pytopyir_test_net_user',
-            'PASSWORD': 'r2+1}aWu~Bk]',
-            'HOST': 'localhost',
-            'POST': '3306',
-            'OPTIONS': {
-                'sql_mode': 'STRICT_ALL_TABLES'
-            }
-        }
-    }
+}
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
@@ -48,7 +26,7 @@ else:
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'assets')
+    os.path.join(BASE_DIR, 'statics')
 ]
 MEDIA_URL = '/media/'
 
@@ -58,47 +36,3 @@ MEDIA_ROOT = "/home/pytopyir/public_html/media"
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn', 'static_root')
 STATIC_ROOT = "/home/pytopyir/public_html/static"
 
-# CORS configs
-CORS_ALLOWED_ORIGINS = [
-
-    'http://pytopy.ir',
-    'https://pytopy.ir',
-    'http://www.pytopy.ir',
-    'https://www.pytopy.ir',
-
-    'http://127.0.0.1:8000',
-]
-CORS_ALLOWED_ORIGIN_REGEXES = [
-    r"^https://\w+\.pytopy\.ir",
-    'http://127.0.0.1:8000',
-
-]
-CORS_ALLOW_METHODS = [
-    'DELETE',
-    'GET',
-    'OPTIONS',
-    'PATCH',
-    'POST',
-    'PUT',
-]
-CORS_ALLOW_HEADERS = [
-    'accept',
-    'accept-encoding',
-    'authorization',
-    'content-type',
-    'dnt',
-    'origin',
-    'user-agent',
-    'x-csrftoken',
-    'x-requested-with',
-]
-
-# REST_FRAMEWORK
-
-REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
-}
