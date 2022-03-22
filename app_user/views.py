db45c0cf87f732db5f04827a3b631d02e4924d07
mohammad sadegh masoumi <sadegh.masoumi.ms@gmail.com>
2022-03-22 23:31:08 +0430

    clean

--- app_user/views.py
+++ app_user/views.py
@@ -9,7 +9,7 @@ from utils.email import EmailService
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from django.http import Http404, JsonResponse
+from django.http import Http404
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 
